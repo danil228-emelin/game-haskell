e912cc862b097e8dd5901ce13f146f8ff29deb06
k18 <danil228aeedj@mail.ru>
2025-02-07 13:21:46 -0500

    add level of dificulties

--- brickbreaker.hs
+++ brickbreaker.hs
@@ -8,14 +8,14 @@ scaleFactor = 20
 
 type Position = (Float, Float)  -- Represents a position in 2D space
 type Velocity = (Float, Float)  -- Represents velocity in 2D space
--- Game state: paddle position, ball position, ball velocity, brick positions, score
-type GameState = (Float, Position, Velocity, [Position], Int)
+-- Game state: paddle position, ball position, ball velocity, brick positions, score, difficulty
+type GameState = (Float, Position, Velocity, [Position], Int, String)
 
 main :: IO ()
 main = play FullScreen  -- Display mode 
             white -- Background color
             60   -- Frames per second
-            (0, (-10, -20), (8, 16), (,) <$> [-10.9, -8.9 .. 9.1] <*> [2, 4 .. 8], 0)  -- Initial game state
+            (0, (-10, -20), (8, 16), (,) <$> [-10.9, -8.9 .. 9.1] <*> [2, 4 .. 8], 0, "Normal")  -- Initial game state
             renderGame -- Render function
             handleInput -- Input handler function
             updateGame -- Update function
@@ -29,9 +29,16 @@ createCircle condition colorValue | condition = color colorValue $ thickCircle 3
 createSquare :: Float -> (Float, Float) -> Path
 createSquare length corner@(x, y) = [corner, (x + length, y), (x + length, y + length), (x, y + length), corner]
 
+-- Function to get the speed multiplier based on difficulty
+getSpeed :: String -> Float
+getSpeed "Easy"   = 0.5
+getSpeed "Normal" = 1.0
+getSpeed "Hard"   = 1.5
+getSpeed _        = 1.0  -- Default case
+
 -- Render function
 renderGame :: GameState -> Picture
-renderGame (paddlePos, (ballX, ballY), _, brickPositions, score) = 
+renderGame (paddlePos, (ballX, ballY), _, brickPositions, score, difficulty) = 
   let
     -- Create overlays as Pictures
     loseOverlay  = if ballY < -20 then color red (rectangleSolid 100 100) else mempty
@@ -43,14 +50,19 @@ renderGame (paddlePos, (ballX, ballY), _, brickPositions, score) =
     ball         = translate ballX ballY (circle 1)
     bricks       = foldMap (polygon . createSquare 1.8) brickPositions
 
-    -- Render the score
-    scoreText    = translate 5 15 (scale 0.01 0.01 (text ("Score: " ++ show score)))  -- Adjust position and scale
+    -- Render the score and difficulty
+    scoreText    = translate 5 17 (scale 0.01 0.01 (text ("Score: " ++ show score)))
+    difficultyText    = translate 5 15 (scale 0.01 0.01 (text ("Difficulty: " ++ difficulty)))
+
   in
-    scale scaleFactor scaleFactor $ loseOverlay <> winOverlay <> lastLine <> paddleLine <> ball <> bricks <> scoreText
+    scale scaleFactor scaleFactor $ loseOverlay <> winOverlay <> lastLine <> paddleLine <> ball <> bricks <> scoreText <> difficultyText
 
 -- Input handler function
 handleInput :: Event -> GameState -> GameState
-handleInput (EventMotion (paddleX, _)) (_, ballPos, ballVel, brickPositions, score) = (paddleX / scaleFactor, ballPos, ballVel, brickPositions, score)
+handleInput (EventKey (Char 'e') Down _ _) (_, ballPos, ballVel, brickPositions, score, _) = (0, ballPos, ballVel, brickPositions, score, "Easy")
+handleInput (EventKey (Char 'n') Down _ _) (_, ballPos, ballVel, brickPositions, score, _) = (0, ballPos, ballVel, brickPositions, score, "Normal")
+handleInput (EventKey (Char 'h') Down _ _) (_, ballPos, ballVel, brickPositions, score, _) = (0, ballPos, ballVel, brickPositions, score, "Hard")
+handleInput (EventMotion (paddleX, _)) (_, ballPos, ballVel, brickPositions, score, difficulty) = (paddleX / scaleFactor, ballPos, ballVel, brickPositions, score, difficulty)
 handleInput _ currentState = currentState
 
 -- Update function
@@ -60,11 +72,13 @@ updateGame :: Float -> GameState -> GameState
 -- (ballX, ballY): Ball position.
 -- (vX, vY): Ball velocity.
 -- brickPositions: List of bricks
-updateGame elapsedTime (paddlePos, (ballX, ballY), (vX, vY), brickPositions, score) = (paddlePos, (newBallX, newBallY), (newVelX, newVelY), updatedBricks, newScore)
+updateGame elapsedTime (paddlePos, (ballX, ballY), (vX, vY), brickPositions, score, difficulty) = 
+    (paddlePos, (newBallX, newBallY), (newVelX, newVelY), updatedBricks, newScore, difficulty)
   where 
-    -- Calculate new ball position
-    newBallX = ballX + vX * elapsedTime
-    newBallY = ballY + vY * elapsedTime
+    -- Calculate new ball position based on difficulty speed
+    speedMultiplier = getSpeed difficulty
+    newBallX = ballX + vX * elapsedTime * speedMultiplier
+    newBallY = ballY + vY * elapsedTime * speedMultiplier
 
     -- Filter out the bricks that have been hit by the ball
     updatedBricks = filter (\(brickX, brickY) -> brickX > ballX || brickX + 2 < ballX || brickY > ballY || brickY + 2 < ballY) brickPositions
@@ -74,12 +88,6 @@ updateGame elapsedTime (paddlePos, (ballX, ballY), (vX, vY), brickPositions, sco
     -- Bounce on paddle, adjust horizontal velocity
     (newVelX, newVelY) | ballY < -10 && ballY > -11 && ballX > paddlePos - 2 && ballX < paddlePos + 2 = ((ballX - paddlePos) * 10, abs vY)
                        -- Bounce on left and right walls
-                       | ballX < -10 || ballX > 10 = (-abs vX * signum ballX,vY)
+                       | ballX < -10 || ballX > 10 = (-abs vX * signum ballX, vY)
                        | ballY > 10 || brickPositions /= updatedBricks = (vX, -abs vY)
-                       | True = (vX,vY)
-
-
--- interface to choose level of game
--- push up,when break breek
--- new simple game
--- tests
+                       | True = (vX, vY)
